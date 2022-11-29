2ca85b21408542e5521ae22a7902f264288ec98f
Igor Quintanilha <igor.quintanilha@vericode.com.br>
2022-11-28 21:13:37 -0300

    upload de txt

--- DX_Spring/correcao-c1/src/main/java/sptech/correcao01/Correcao01Application.java
+++ DX_Spring/correcao-c1/src/main/java/sptech/correcao01/Correcao01Application.java
@@ -1,12 +1,25 @@
 package sptech.correcao01;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import sptech.correcao01.dominio.ImportacaoService;
+
+import javax.annotation.Resource;
 
 @SpringBootApplication
-public class Correcao01Application {
+public class Correcao01Application implements CommandLineRunner {
+
+	@Resource
+	ImportacaoService storageService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Correcao01Application.class, args);
+	}
+
+	@Override
+	public void run(String... arg) throws Exception {
+//    storageService.deleteAll();
+		storageService.init();
 	}
 }
